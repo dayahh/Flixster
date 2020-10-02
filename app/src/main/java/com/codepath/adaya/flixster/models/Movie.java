0ed6fc3f913f17b6e354141857ee1d5c6e71b420
Adaya Head <ahead9@my.westga.edu>
2020-10-02 05:13:48 -0400

    Expose details of movie (ratings using RatingBar, popularity, and synopsis) in a separate activity, also allowed video posts to be played in full-screen using the YouTubePlayerView.

--- app/src/main/java/com/codepath/adaya/flixster/models/Movie.java
+++ app/src/main/java/com/codepath/adaya/flixster/models/Movie.java
@@ -3,21 +3,30 @@ package com.codepath.adaya.flixster.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
 
+    int movieId;
     String posterPath;
     String title;
-
     String overview;
+    double rating;
+
+    // empty constructor needed by the Parceler library
+    public Movie() {
+    }
 
     public Movie(JSONObject jsonObject) throws JSONException {
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -29,7 +38,7 @@ public class Movie {
     }
 
     public String getPosterPath() {
-        return posterPath;
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
     }
 
     public String getTitle() {
@@ -38,6 +47,14 @@ public class Movie {
 
     public String getOverview() {
         return overview;
+    }
+
+    public double getRating(){
+        return rating;
+    }
+
+    public int getMovieId(){
+        return movieId;
     }
 
 }
